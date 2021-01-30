42673e79c95ee1287035f91ab238eec3ba5cf2d1
pavelborisovich <73785960+pavelborisovich@users.noreply.github.com>
2021-01-31 00:27:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,12 @@ sudo mv Telegram /usr/bin/
 cd ..
 rm tsetup.2.5.8.tar.xz
 rm Telegram
+Telegram &
 sudo add-apt-repository ppa:teejee2008/ppa
 sudo apt update
 sudo apt install timeshift
 sudo apt install vlc plank transmission xfce4-terminal gnome-disk-utility -y
+plank &
 sudo systemctl /usr/bin/plank
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
